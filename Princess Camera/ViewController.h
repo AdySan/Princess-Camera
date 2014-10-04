5eb84ffd689528679eae15063b875e7df1aa8698
Aditya Tannu <adityatannu@Adityas-Mac-mini.local>
2014-10-04 09:12:31 -0700

    Initial Commit

---
+++ Princess Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Princess Camera
+//
+//  Created by Aditya Tannu on 10/4/14.
+//  Copyright (c) 2014 Aditya Tannu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
